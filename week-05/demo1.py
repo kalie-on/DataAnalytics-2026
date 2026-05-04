dc3fe8a6ef2baf9bf6f53287d0190a5b27356a13
ndifon caleb ndakina <calebndifon2@gmail.com>
2026-05-04 16:03:45 -0400

    class demo

---
+++ week-05/demo1.py
@@ -0,0 +1,17 @@
+# Ndifon Caleb N
+# 5/4/2026
+
+name = "Caleb" 
+age = 22
+
+print(name)
+print(age)
+
+print(f'my name is {name} and i am {age}')
+
+pi = 3.145678
+
+print(f"the value of pi is {pi:.2f}")
+
+sale = 12345.7685
+print(f"the value of sale is ${sale:,.2f}")
