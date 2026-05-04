1158a46c36013802d5d2dd7abee958c4e2a09d72
ndifon caleb ndakina <calebndifon2@gmail.com>
2026-05-04 16:36:54 -0400

    homework

---
+++ week-05/Ex1A_hello.py
@@ -0,0 +1,17 @@
+print('hello world!')
+
+message = ('Hello World!')
+print(message)
+
+# Hello word is printed twice because from the print command and the printed again using the variable message
+
+# Displaying dollars and cents
+
+dollar = 3
+cents = 0.50
+print(dollar + cents)
+
+d_str = "3 dollars"  
+c_str = "50 cents" 
+
+print(d_str + " " + c_str)
