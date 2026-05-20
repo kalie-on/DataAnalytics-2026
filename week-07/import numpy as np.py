642a732af6dc981aea0093477e92471a63d6fc1e
ndifon caleb ndakina <calebndifon2@gmail.com>
2026-05-20 16:41:59 -0400

    updates

---
+++ week-07/import numpy as np.py
@@ -0,0 +1,19 @@
+import numpy as np
+#1D
+arr1 = np.array([1,2,3])
+print(arr1)
+#2D
+arr2 = ([[1,2,3], [1,2,3,], [1,2,3]])
+print(arr2)
+#3D
+arr3 = ([[[1,2,3], [1,2,3]],[[1,2,3],[1,2,3]]])
+print(arr3)
+
+
+arrA = ([[11,12,13],[14,15,16]])
+print(arrA)
+for x in arrA:
+    print(x)
+    for y in arrA:
+        print(y)
+        
